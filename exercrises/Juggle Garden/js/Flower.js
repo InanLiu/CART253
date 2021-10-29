e80fd62ea24552ca16c648f89d27ecd7a60976b6
InanLiu <90408159+InanLiu@users.noreply.github.com>
2021-10-29 13:40:57 +0800

    nearly finish

--- exercrises/Juggle Garden/js/Flower.js
+++ exercrises/Juggle Garden/js/Flower.js
@@ -1,32 +1,71 @@
 
 
-class Flower{
+   class Flower{
 
-   constructor(){
+   constructor(x, y, size, stemLength, petalColor){
       //p and size of flower
-     this.x =  random(0, width);
-     this.y =  random(0, height);
-     this.size =  50;
-     this.stemLength = 75;
+     this.x =  x;
+     this.y =  y;
+     this.size =  size;
+     this.maxSize = size;
+     this.stemLength = stemLength;
      this.stemThickness = 10;
-     this.patalThickness = 10;
+     this.petalThickness = 10;
+     this.maxPetalthickness = 10;
      //colour
      this.stemColor = {
-       r = 50;
-       g = 150;
-       b = 50;
-     };
-     this.petalColor = {
-       r = 50;
-       g = 150;
-       b = 50;
-     };
+       r : 50,
+       g : 150,
+       b : 50
+     }
+     this.petalColor = petalColor
      this.centreColor = {
-       r = 50;
-       g = 150;
-       b = 50;
+       r : 50,
+       g : 0,
+       b : 0
      };
-
+     this.alive = true;
    }
 
-}
+    shrink() {
+      let shrinkage = random(0, 0.1);
+      this.size -= shrinkage;
+      this.petalThickness = this.petalThickness - shrinkage/10;
+
+      if(this.size <= 0 || this.petalThickness <= 0){
+        this.alive = false;
+      }
+    }
+
+    pollinate(){
+      let growth = random(0,0.5)
+      this.size = this.size + growth
+      this.petalThickness =   this.petalThickness + growth / 10;
+
+      this.size = constrain(this.size,0,this.maxSize);
+      this.petalThickness = constrain(this.petalThickness,0,this.maxPetalthickness)
+
+    }
+    display(){
+      push();
+      //draw stem
+      strokeWeight(this.stemThickness);
+      stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+      line(this.x, this.y, this.x, this.y + this.stemLength);
+      //drawing flowers
+      strokeWeight(this.petalThickness);
+      fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+      stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+      ellipse(this.x, this.y, this.size);
+      pop();
+    }
+
+    mousePressed(){
+      let d = dist(this.x,this.y,mouseX,mouseY);
+      if (d < this.size/2 + this.petalThickness){
+        this.stemLength = this.stemLength + 5;
+        this.y = this.y - 5;
+      }
+    }
+
+  }
--- exercrises/Juggle Garden/js/script.js
+++ exercrises/Juggle Garden/js/script.js
@@ -11,12 +11,16 @@ let garden = {
   flowers: [],
   // How many flowers in the garden
   numFlowers: 20,
+  //an array of bees
+  bees:[],
+  //Howmany bees in the garden
+  numBees:7,
   //background colour
   grassColor:{
     r:120,
     g:180,
     b:120,
-  };
+  }
 }
 // setup()
 //
@@ -25,44 +29,59 @@ function setup() {
   createCanvas(600,600);
 
   //create flowers by for loop
-  for(let i = 0; i < garden.numFowers; i++){
+  for(let i = 0; i < garden.numFlowers; i++){
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50, 80);
+    let stemLength = random(50,100);
+    let petalColor ={
+      r: random(100, 255),
+      g: random(100, 255),
+      b: random(100, 255),
+    }
     //creat a new flower
-    let flower = createFlower();
+    let flower = new Flower(x, y, size, stemLength, petalColor);
+    //add the flower to the array
     garden.flowers.push(flower);
   }
-}
 
-function createFlower(){
-  let flower = {
-    //p and size of flower
-    x: random(0, width),
-    y: random(0, height),
-    size: 50,
-    stemLength:75,
-    stemThickness:10,
-    patalThickness:10,
-    //colour
-    stemColor:{
-      r:50,
-      g:150,
-      b:50,
-    },
-    petalColor:{
-      r:50,
-      g:150,
-      b:50,
-    },
-    centreColor:{
-      r:50,
-      g:150,
-      b:50,
-    },
-  };
-  return flowe;
+  //create the bees
+  for (let i = 0; i < garden.numBees; i++){
+    let bee = new Bee(random(0, width),random(0,height))
+    garden.bees.push(bee);
+  }
 }
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+    background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b)
+
+    for (let i = 0; i < garden.flowers.length; i++){
+      let flower = garden.flowers[i];
+      if (flower.alive){
+      flower.display();
+      flower.shrink();
+    }
+  }
+    for (let i = 0; i < garden.bees.length; i++){
+      let bee = garden.bees[i]
+      if (bee.alive){
+        bee.shrink();
+        bee.move();
+        bee.display();
+
+        for (let j = 0; j < garden.flowers.length; j++){
+          let flower = garden.flowers[j];
+          bee.tryToPollinate(flower);
+        }
+      }
+    }
 
 }
+function mousePressed(){
+  for (let i = 0; i < garden.flowers.length; i++){
+    let flower = garden.flowers[i];
+    flower.mousePressed();
+  }
+ }
