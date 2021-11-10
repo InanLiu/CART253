7bc76982a4231f8697d9da59f86a563c2cdf35a0
InanLiu <90408159+InanLiu@users.noreply.github.com>
2021-11-10 12:48:29 +0800

    elements prepare(with video

--- exercrises/sounds/sounds/js/script.js
+++ exercrises/sounds/sounds/js/script.js
@@ -7,16 +7,27 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let barkSFX;
+
+function preload(){
+  // soundFormats('mp3', 'ogg')
+   barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+   createCanvas(600,600);
+   userStartAudio();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+}
 
+function mousePressed(){
+   barkSFX.play();
 }
