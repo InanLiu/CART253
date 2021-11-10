2324c15e3173092982605e47b740377eb0dcc453
InanLiu <90408159+InanLiu@users.noreply.github.com>
2021-11-11 06:57:39 +0800

    elements of p2

---
+++ projects/click.hero/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(400, 400);
+
+   // Create our graphics object (like a canvas)
+   drawingArea.graphics = createGraphics(drawingArea.width, drawingArea.height);
+   // Set its background to black only once, so we get a drawing effect
+   drawingArea.graphics.background(0)
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
