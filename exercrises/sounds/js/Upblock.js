68b15c74ce1e240104d0e5d62ad721bed53e480b
InanLiu <90408159+InanLiu@users.noreply.github.com>
2021-11-11 18:09:30 +0800

    try out of Polymorphism and Inheritance

--- exercrises/sounds/js/Upblock.js
+++ exercrises/sounds/js/Upblock.js
@@ -1,11 +1,11 @@
-class Upbolck extends Block{
+class Upbolck extends Blocks{
   constructor(x,y,blockH){
     super(x,y,blockH)
     this.width = 20;
     this.vx = 1;
 
   }
-  display(){ // display the upblock 
+  display(){ // display the upblock
     push();
     rectMode(CENTER);
     noStroke();
