e9f09ece9a5007c73c3bd277e11887a9b26be851
InanLiu <90408159+InanLiu@users.noreply.github.com>
2021-11-14 19:55:56 +0800

    colourful brush introduce

--- projects/painting simulation/js/script.js
+++ projects/painting simulation/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let cars = [];
-let numCars = 10;
+let state = colourfulBrush;
+//random dot for the water ink state
 let colorfilling = [`rgba(228,138,160,0.5)`,`rgba(59,134,122,0.5)`,`rgba(57,79,133,0.5)`,
   `rgba(139,158,154,0.5)`,`rgba(169,188,167,0.5,)`,`rgba(245,210,83,0.5)`]
 // setup()
@@ -10,25 +10,41 @@ let colorfilling = [`rgba(228,138,160,0.5)`,`rgba(59,134,122,0.5)`,`rgba(57,79,1
 function setup() {
   createCanvas(windowWdith, windowHeight);
   background(230);
-  // for (let i = 0; i < numCars; i++){
-  //   let x = random(0,width);
-  //   let y = random(0,height);
-  //   let car = new Car(x,y);
-  //   cars.push(car)
+  noStroke();
+)
   }
 }
 // draw()
 //
 // Description of draw() goes here.
+
+// shall be at least of 3 stages to creative painting
 function draw() {
+  //
+  if (state === `beginningLine`){
+     beginningLine()
+   }
+   else if (state === `waterInk`){
+     start();
+   }else if (state === `colourfulBrush`){
+     colourfulBrush();
+   }else if (state === `end1`){
+     end1();
+   }else if (state === `end2`){
+     end2();
+   }
 
 
- // for (let i = 0; i < cars.length; i++){
- //   let car = cars[i];
- //   car.move();
- //   car.wrap();
- //   car.display();
- // }
+
+
+}
+function colourfulBrush(){
+  push()
+  fill()
+  colorMode(HSB)
+  fill(frameCout)
+  ellipse(mouseX, mouseY, 30, 30 )
+  pop()
 }
 // start different brushes
 function mousePressed(){
